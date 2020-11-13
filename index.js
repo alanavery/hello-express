3d768683230e7ba9afabce13418d44af5f16a033
delayedaa <delayedaa@gmail.com>
2020-11-13 11:41:04 -0600

    [index.js] Add /about and /blog

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ app.get('/', (req, res) => {
   res.send('Hi, Alan!');
 });
 
-app.get('/goodbye', (req, res) => {
-  res.send('Goodbye, Alan!');
+app.get('/about', (req, res) => {
+  res.send('This is the About page.');
 });
 
-app.get('/goodbye/silly', (req, res) => {
+app.get('/blog', (req, res) => {
+  res.send('Welcome to my blog.');
+});
+
+app.get('/about/silly', (req, res) => {
   res.send('This is getting ridiculous.');
 });
 
